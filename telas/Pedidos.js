00cb35417bba91b0ce6d166d1af14f0ebbb0cb35
BrunoJoGomes <bruno.jogomes@senacsp.edu.br>
2025-06-03 00:09:37 -0300

    CRUD

--- telas/Pedidos.js
+++ telas/Pedidos.js
@@ -1,63 +1,119 @@
-import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity, Modal, ScrollView,Image } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { db } from '../FirebaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs} from 'firebase/firestore';
+import { DrawerItemList } from '@react-navigation/drawer';
 
 export default function Pedidos({ navigation }) {
+  const [itensCardapio,setItensCardapio] = useState([]);
   const [endereco, setEndereco] = useState('');
-  const [itens, setItens] = useState('');
+  const [itensPedido, setItensPedido] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
 
+
+  const carregarCardapio = async () => {
+    try {
+      const colecoes = ['Lanches', 'Combos', 'Bebidas', 'Doces','Promoções'];
+      let itensTotais = [];
+
+      for (const nomeColecao of colecoes) {
+        const snapshot = await getDocs(collection(db, nomeColecao));
+        const itens = snapshot.docs.map(doc => ({
+          id: doc.id,
+          titulo: doc.titulo,
+          preco : doc.preco,
+          imagem: doc.imagem,
+          ...doc.data()
+        }));
+        itensTotais = [...itensTotais, ...itens];
+      }
+
+      setItensCardapio(itensTotais);
+    } catch (error) {
+      console.error('Erro ao carregar cardápio:', error);
+    }
+  };
+
+  const adicionarItemAoPedido = (item) => {
+    setItensPedido([...itensPedido,item]);
+  }
+
+  const removerItem = (index) => {
+    const novaLista = [...itensPedido];
+    novaLista.splice(index, 1);
+    setItensPedido(novaLista);
+  };
+
+
+
   const adicionarPedido = async () => {
-    if (!endereco || !itens) return;
+    if (!endereco || itensPedido.length === 0) return;
 
     try {
       await addDoc(collection(db, 'Pedidos'), {
         endereco,
-        itens,
+        itens: itensPedido,
       });
       setEndereco('');
-      setItens('');
+      setItensPedido([]);
       setModalVisible(false);
+      alert('Pedido salvo com sucesso!')
     } catch (error) {
       console.error("Erro ao adicionar pedido: ", error);
     }
   };
 
+  useEffect(() => {
+    carregarCardapio();
+  }, []);
+
   return (
     <SafeAreaView style={{ backgroundColor: 'green', flex: 1 }}>
-      <TouchableOpacity onPress={() => setModalVisible(true)}>
-        <Text style={{ color: 'white', fontSize: 18, margin: 20 }}>+ Adicionar Pedido</Text>
-      </TouchableOpacity>
+      <ScrollView>
+        <Button title="Novo Pedido" onPress={()=> setModalVisible(true)}/>
+        <Modal visible={modalVisible} animationType="slide">
+          <View>
+            <Text>Novo Pedido</Text>
+            <TextInput
+              placeholder="Endereço"
+              value={endereco}
+              onChangeText={setEndereco}
 
-      <Modal visible={modalVisible} animationType="slide">
-        <View style={{ padding: 20 }}>
-          <TextInput
-            placeholder="Endereço"
-            value={endereco}
-            onChangeText={setEndereco}
-            style={{ borderBottomWidth: 1, marginBottom: 15 }}
-          />
-          <TextInput
-            placeholder="Itens"
-            value={itens}
-            onChangeText={setItens}
-            multiline
-            style={{ borderBottomWidth: 1, marginBottom: 15 }}
-          />
-          <Button title="Salvar" onPress={adicionarPedido} />
-          <Button
-            title="Cancelar"
-            color="gray"
-            onPress={() => {
-              setModalVisible(false);
-              setEndereco('');
-              setItens('');
-            }}
-          />
-        </View>
-      </Modal>
+            />
+            
+            <Text>Itens do Cardápio:</Text>
+              <FlatList
+                data={itensCardapio}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <TouchableOpacity onPress={() => adicionarItemAoPedido(item)}>
+                    <Text>{item.titulo}</Text>
+                    <Image source={{ uri: item.imagem }} style={{ width: 50, height: 50 }}/>
+                    <Text>R$ {item.preco}</Text>
+                  </TouchableOpacity>
+                )}
+                numColumns={4}
+                columnWrapperStyle={{ justifyContent: 'space-around' }}
+              />
+
+            <Text>Itens Selecionados:</Text>
+            <FlatList
+              data={itensPedido}
+              keyExtractor={(_, index) => index.toString()}
+              renderItem={({ item, index }) => (
+                <View>
+                  <Text>{item.nome} - R$ {item.preco}</Text>
+                  <Button title="Remover" color="red" onPress={() => removerItem(index)} />
+                </View>
+              )}
+            />
+
+            <Button title="Salvar Pedido" onPress={adicionarPedido} />
+            <Button title="Cancelar" color="gray" onPress={() => setModalVisible(false)} />
+          </View>
+        </Modal>
+      </ScrollView>
     </SafeAreaView>
   );
 }
